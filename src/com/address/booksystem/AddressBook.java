6ebc0a92683a8bb25fecf8e88cb5b4b3d8e85631
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-10-23 00:05:43 +0530

    Added: UC2 Ability to add a new

---
+++ src/com/address/booksystem/AddressBook.java
@@ -0,0 +1,14 @@
+package com.address.booksystem;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    public ArrayList<Details> contactList = new ArrayList<>();
+
+    public  ArrayList<Details> contactList(Details d){
+        contactList.add(d);
+        return contactList;
+    }
+
+}
